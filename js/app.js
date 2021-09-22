a72a25500a283e2ac7c392202b37b3df7d1f0da5
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2021-09-22 11:54:47 -0300

    rezise tab-content when form-menu is shown

--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
 let formMenuToggle = false;
 let countryMenuToggle = false;
+let tabContentHeight = document.getElementById("tab-content").offsetHeight;
 
 const dropDownMenu = (clickedElement) => {
-    console.log("clicked Element");
+    
+    let formHeight;
+
     if (clickedElement === "place-and-contact-form" && formMenuToggle === false) {
-        console.log("place-and-contact-form");
+        setTimeout(() => { // se suma a tab-content el tamaño de place-and-contact-form
+            formHeight = document.getElementById("place-and-contact-form").offsetHeight;
+            document.getElementById("tab-content").style.height = (tabContentHeight + formHeight) + 'px';
+        }, 10);
         document.getElementById("place-and-contact-form").style.display = "block";
         formMenuToggle = true;
     } else if (clickedElement === "place-and-contact-form" && formMenuToggle === true) {
-        document.getElementById("place-and-contact-form").style.display = "none";
+         // vuelve tab-content a su tamaño inicial
+        formHeight = document.getElementById("place-and-contact-form").offsetHeight;
+        document.getElementById("tab-content").style.height = tabContentHeight + 'px';
+        setTimeout(() => {
+            document.getElementById("place-and-contact-form").style.display = "none";
+        }, 10);
         formMenuToggle = false;
     } else if (clickedElement === "country-menu-ul" && countryMenuToggle === false) {
-        document.getElementById("country-menu-ul").style.display = "none";
+        document.getElementById("country-menu-ul").style.display = "block";
         countryMenuToggle = true;
     } else if (clickedElement === "country-menu-ul" && countryMenuToggle === true) {
-        document.getElementById("country-menu-ul").style.display = "block";
+        document.getElementById("country-menu-ul").style.display = "none";
         countryMenuToggle = false;
     }
 }
