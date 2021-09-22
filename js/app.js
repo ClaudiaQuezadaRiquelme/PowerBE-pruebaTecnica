0e80ff08073d2aba3c27201effc0fc2b88560204
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2021-09-22 09:09:17 -0300

    dropDownMenu js for country-menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let formMenuToggle = false;
+let countryMenuToggle = false;
 
 const dropDownMenu = (clickedElement) => {
     console.log("clicked Element");
@@ -9,5 +10,11 @@ const dropDownMenu = (clickedElement) => {
     } else if (clickedElement === "place-and-contact-form" && formMenuToggle === true) {
         document.getElementById("place-and-contact-form").style.display = "none";
         formMenuToggle = false;
+    } else if (clickedElement === "country-menu-ul" && countryMenuToggle === false) {
+        document.getElementById("country-menu-ul").style.display = "none";
+        countryMenuToggle = true;
+    } else if (clickedElement === "country-menu-ul" && countryMenuToggle === true) {
+        document.getElementById("country-menu-ul").style.display = "block";
+        countryMenuToggle = false;
     }
 }
